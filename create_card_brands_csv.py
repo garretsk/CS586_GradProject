9c6510d762a7e5f848a2f6afcb089e243ffe283f
Garret Katayama <garret.katayama@gmail.com>
2024-11-16 14:06:26 -0800

    Python scripts to create card_brand and card_type CSVs

--- create_card_brands_csv.py
+++ create_card_brands_csv.py
@@ -1,1 +1,17 @@
 import pandas as pd
+
+input_file = "raw_data/cards_data.csv"
+output_file = "table_data/card_brands.csv"
+
+card_brands = set()
+chunk_size = 10000
+with pd.read_csv(input_file, chunksize=chunk_size) as csv_reader:
+    for chunk in csv_reader:
+        for _, row in chunk.iterrows():
+            card_brands.add(row["card_brand"])
+
+dataframe = pd.DataFrame(columns=["Id", "CardBrand"])
+for card_type in card_brands:
+    dataframe.loc[len(dataframe)] = [len(dataframe), card_type]
+
+dataframe.to_csv(output_file, index=False)
--- create_card_types_csv.py
+++ create_card_types_csv.py
@@ -1,1 +1,17 @@
 import pandas as pd
+
+input_file = "raw_data/cards_data.csv"
+output_file = "table_data/card_types.csv"
+
+card_types = set()
+chunk_size = 10000
+with pd.read_csv(input_file, chunksize=chunk_size) as csv_reader:
+    for chunk in csv_reader:
+        for _, row in chunk.iterrows():
+            card_types.add(row["card_type"])
+
+dataframe = pd.DataFrame(columns=["Id", "CardType"])
+for card_type in card_types:
+    dataframe.loc[len(dataframe)] = [len(dataframe), card_type]
+
+dataframe.to_csv(output_file, index=False)
