6e1bb7a4bdac9899686ea714b42813374e3e1020
Garret Katayama <garret.katayama@gmail.com>
2024-12-04 18:23:54 -0800

    Multiprocessing script combined into original create_hastransaction_csv.py script

--- create_hastransaction_csv.py
+++ create_hastransaction_csv.py
@@ -1,22 +1,32 @@
 import pandas as pd
+import multiprocessing
 
 input_file = "raw_data/transactions_data.csv"
 output_file = "table_data/hastransaction.csv"
 
-dataframe = pd.DataFrame(columns=["CardId", "MerchantId", "Date", "Amount", "UseChip", "Zip"])
+def process_chunk(chunk):
+    print("Reading in CSV - " + multiprocessing.current_process().name + ", PID: " + str(multiprocessing.current_process().pid) + " starting a new chunk")
+    rows = []
+    for _, row in chunk.iterrows():
+        card_id = row["card_id"]
+        merchant_id = row["merchant_id"]
+        date = row["date"]
+        amount = row["amount"]
+        use_chip = row["use_chip"]
+        zipcode = row["zip"]
+        rows.append([card_id, merchant_id, date, amount, use_chip, zipcode])
+    chunk_dataframe = pd.DataFrame(rows, columns=["CardId", "MerchantId", "Date", "Amount", "UseChip", "Zip"])
+    return chunk_dataframe
 
-chunk_size = 10000
-with pd.read_csv(input_file, chunksize=chunk_size) as csv_reader:
-    for chunk in csv_reader:
-        print("Reading in CSV - starting a new chunk")
-        for _, row in chunk.iterrows():
-            card_id = row["card_id"]
-            merchant_id = row["merchant_id"]
-            date = row["date"]
-            amount = row["amount"]
-            use_chip = row["use_chip"]
-            zipcode = row["zip"]
 
-            dataframe.loc[len(dataframe)] = [card_id, merchant_id, date, amount, use_chip, zipcode]
-            
-dataframe.to_csv(output_file, index=False)
+if __name__ == "__main__":
+    chunk_size = 100000
+    with pd.read_csv(input_file, chunksize=chunk_size) as csv_reader:
+        chunks = list(csv_reader)
+
+    with multiprocessing.Pool() as pool:
+        print("Beginning CSV processing")
+        results = pool.map(process_chunk, chunks)
+
+    dataframe = pd.concat(results, ignore_index=True)
+    dataframe.to_csv(output_file, index=False)
